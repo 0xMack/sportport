418649c32700e737ef814c075348ddfd3ecb964d
Mack Preston <mack.preston42@gmail.com>
2017-11-26 13:03:50 -0400

    Deleting config file

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'hostname' => '',
-    'username' => '',
-    'password' => '',
-    'dbname' => ''
-    // This password will be left in the git history on purpose
-    // It's just not worth creating an external config for this assignment
-);
--- php/database.php
+++ php/database.php
@@ -2,6 +2,7 @@
 
 function connect_db(){
     // db configuration
+
     $configs = include('config.php');
     $db_hostname = $configs['hostname'];
     $db_username = $configs['username'];

9ec146357c7c4316e436aef3ff2810658b82a92b
Mack Preston <mack.preston42@gmail.com>
2017-11-30 18:28:44 -0400

    Got rid of unused js and css

--- index.php
+++ index.php
@@ -17,11 +17,9 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-    <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
+    <link href="css/freelancer.css" rel="stylesheet">
 
   </head>
   <?php
@@ -59,7 +57,7 @@
     <header class="masthead bg-primary text-white text-center">
       <div class="container">
         <img class="img-fluid mb-5 d-block mx-auto" src="img/football.jpg" alt="soccer picture">
-        <h1 class="text-uppercase mb-0">Sports Port</h1>
+        <h1 class="text-uppercase mb-0">Sport Port</h1>
         <hr class="star-light">
         <h2 class="font-weight-light mb-0">Join Leagues - Make Teams - Keep Track of Your Stats</h2>
       </div>
@@ -154,7 +152,6 @@
 
     <!-- Plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
     <!-- Contact Form JavaScript -->
     <script src="js/jqBootstrapValidation.js"></script>

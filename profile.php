5c7999b8ad5de211d54433393612583498ae711c
Mack Preston <mack.preston42@gmail.com>
2017-12-01 17:19:23 -0400

    profile fixes

--- profile.php
+++ profile.php
@@ -4,8 +4,8 @@
     <section style="margin-top:50px;
                 /*margin-right: 140px;*/
                 /*margin-left: 140px;*/
-                background-color:seagreen;">
-        <div style="">
+                background-color:#2EA6C4;">
+        <div>
             <img src ="img/profilePic.png" alt="profilePic" width="175" height="175" style="float: left; margin-right: 20px; position: relative; left:10px; border: 1px solid black; border-radius: 50%">
             <?php
                 echo "<h1 style='color:white;'>$firstName $lastName</h1>";
@@ -24,13 +24,13 @@
             </div>
             <div>
                 <img src="img/Team.png" width="100" height="100" >
-                <a href="myteams.php" style=" font: italic bold 12px/30px Georgia, serif;float:inherit; font-size: 20px;link {color: cornflowerblue;
+                <a href="myteams.php" style=" font: italic bold 12px/30px Georgia, serif;float:inherit; font-size: 20px;link {color: #000000 !important;
     background-color: transparent;
-    text-decoration: none;}   "> &#9830; My Team</a>
+    text-decoration: none;}   ">My Team</a>
                 <img src="img/cup.png" width="100" height="100" style="margin-left: 400px;">
-                <a href="leagues.php?sport=soccer" style=" font: italic bold 12px/30px Georgia, serif;float:inherit; font-size: 20px;link {color: cornflowerblue;
+                <a href="leagues.php?sport=soccer" style=" font: italic bold 12px/30px Georgia, serif;float:inherit; font-size: 20px;link {color: #000000 !important;
     background-color: transparent;
-    text-decoration: none;}   "> &#9830; Leagues</a>
+    text-decoration: none;}   ">Leagues</a>
             </div>
         </div>
 <!--    </section>-->

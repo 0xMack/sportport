b7e714ae0e8475f5b42f58ecbc9b7d552148c5d6
ian-dawson <ian.dawson@dal.ca>
2017-12-09 18:16:00 -0400

    modifications to myteams

--- php/database.php
+++ php/database.php
@@ -87,7 +87,7 @@ function get_teams($conn, $leagueID, $userID){
     else{
         //Get all teams for a user
         //$stmt = $conn->prepare("SELECT * from team_membership WHERE UserID=?");
-        $stmt = $conn->prepare("SELECT * from team_membership INNER JOIN teams ON team_membership.TeamID=teams.TeamID INNER JOIN leagues ON teams.LeagueID=leagues.LeagueID INNER JOIN sports ON leagues.SportID=sports.SportID WHERE UserID=?");
+        $stmt = $conn->prepare("SELECT DISTINCT * from team_membership INNER JOIN teams ON team_membership.TeamID=teams.TeamID INNER JOIN leagues ON teams.LeagueID=leagues.LeagueID INNER JOIN sports ON leagues.SportID=sports.SportID WHERE UserID=?");
         $stmt->execute([$userID]);
     }
     $teams = $stmt->fetchAll();
